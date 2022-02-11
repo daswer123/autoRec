7bf2cea7592752b4d7aafa96d6b0bbb73d706334
Danil Boldyrev <daswerq123@gmail.com>
2022-02-11 05:07:19 +0300

    car init and update login

--- src/js/app.js
+++ src/js/app.js
@@ -4,15 +4,9 @@ const btnSwitchLogin = document.querySelector(".auth--login .auth__change");
 const btnSwithRegister = document.querySelector(".auth--register .auth__change");
 
 const changeForm = (e) => {
-  // e.preventDefault();
-  console.log(register.classList, login.classList);
-  console.log("click");
-
   login.classList.toggle("active");
   register.classList.toggle("active");
 };
-
-// changeForm();
 
 btnSwitchLogin.addEventListener("click", changeForm);
 btnSwithRegister.addEventListener("click", changeForm);
