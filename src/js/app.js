55ed29ba9afd723f27ef1c236926de204c26edac
Danil Boldyrev <daswerq123@gmail.com>
2022-02-12 00:42:18 +0300

    add finance form and create new car + firebase init

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,22 @@
-const login = document.querySelector(".auth--login");
-const register = document.querySelector(".auth--register");
+import { auth, app, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "./firebase.js";
+
+// APP LOGIC
+
+const loginForm = document.querySelector(".auth--login");
+const registerForm = document.querySelector(".auth--register");
 const btnSwitchLogin = document.querySelector(".auth--login .auth__change");
 const btnSwithRegister = document.querySelector(".auth--register .auth__change");
 
 const changeForm = (e) => {
-  login.classList.toggle("active");
-  register.classList.toggle("active");
+  loginForm.classList.toggle("active");
+  registerForm.classList.toggle("active");
 };
 
 btnSwitchLogin.addEventListener("click", changeForm);
 btnSwithRegister.addEventListener("click", changeForm);
+
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const login = "daswerq123@gmail.com";
+  const pass = "123123";
+});
---
+++ src/js/firebase.js
@@ -0,0 +1,57 @@
+//FIREBASE
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+// import { getAnalytics } from "firebase/";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDCgKn5drHD0kL6RawTiY3LNVw6tEQUJLo",
+  authDomain: "todoapp-d8181.firebaseapp.com",
+  databaseURL: "https://todoapp-d8181.firebaseio.com",
+  projectId: "todoapp-d8181",
+  storageBucket: "todoapp-d8181.appspot.com",
+  messagingSenderId: "524163508174",
+  appId: "1:524163508174:web:0ef5ddf9686c221389ad9f",
+  measurementId: "G-B25DJHT6C9",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(); // Инстанс текущей авторизации
+
+// createUserWithEmailAndPassword(auth, login, pass) // Создать пользователя
+//   .then((userCredential) => {
+//     // Signed in
+//     const user = userCredential.user;
+//     console.log("Created");
+//     // ...
+//   })
+//   .catch((error) => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+//     console.log("Нету такого");
+//     // ..
+//   });
+
+//   signInWithEmailAndPassword(auth, login, pass) // Авторизоватся
+//     .then((userCredential) => {
+//       // Signed in
+//       const user = userCredential.user;
+//       console.log("Вошел!", user);
+//       // ...
+//     })
+//     .catch((error) => {
+//       const errorCode = error.code;
+//       const errorMessage = error.message;
+//       console.log("Нету такого");
+//     });
+
+//   console.log("Sended");
+
+export { app, auth, createUserWithEmailAndPassword, signInWithEmailAndPassword };
