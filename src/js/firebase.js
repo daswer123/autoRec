854569e07b6b3cf87f377b3cf1614070aa74687e
Danil Boldyrev <daswerq123@gmail.com>
2022-02-22 19:25:55 +0300

    Подключение Firebase
    Подготовка к react

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -3,9 +3,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
-// import { getAnalytics } from "firebase/";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -22,7 +19,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 const auth = getAuth(); // Инстанс текущей авторизации
 
 // createUserWithEmailAndPassword(auth, login, pass) // Создать пользователя
